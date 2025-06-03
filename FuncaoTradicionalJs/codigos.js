d7ce9096064383a57193da1ca8f79a54ff1d2522
Rodolfo <rodolfoalexandre2009@gmail.com>
2025-06-03 10:51:20 -0300

    codigos.js

---
+++ FuncaoTradicionalJs/codigos.js
@@ -0,0 +1,119 @@
+// 1 Crie uma função que exibe a mensagem &quot;Olá, mundo!&quot; no console.
+function OlaMundo() {
+console.log(&quot;Olá, mundo&quot;)
+}
+OlaMundo()
+// 2 Crie uma função chamada mostrarNome que exibe seu nome completo.
+function MostrarNome() {
+console.log(&quot;Rodolfo Alexandre de Andrade&quot;)
+}
+MostrarNome()
+// 3 Crie uma função chamada mostrarIdade que exibe sua idade.
+function MostrarIdade() {
+console.log(&quot;16&quot;)
+}
+MostrarIdade()
+// 4 Crie uma função chamada somarDoisNumeros que soma dois números fixos.
+function SomarDoisNumeros() {
+let resultado = 10 + 11
+console.log(&quot;Resultado da soma:&quot;, resultado)
+}
+SomarDoisNumeros()
+// 5 Crie uma função chamada mostrarMensagem que exibe uma mensagem motivacional.
+function MostrarMensagem() {
+console.log(&quot;Não Desista&quot;)
+}
+MostrarMensagem()
+// 6 Crie uma função chamada mostrarData que exibe a data de hoje.
+function MostrarData() {
+console.log(&quot;06/05/2025&quot;)
+}
+MostrarData()
+
+// 7 Crie uma função chamada mostrarCidade que exibe o nome de uma cidade.
+function MostrarCidade() {
+console.log(&quot;Ferraz de Vasconcelos&quot;)
+}
+MostrarCidade()
+// 8 Crie uma função que receba um nome como parâmetro e exiba “Bem-vindo, [nome]!”
+function BemVindo() {
+let nome = prompt(&quot;Digite seu nome: &quot;)
+console.log(&quot;Bem-vindo, &quot; + nome + &quot;!&quot;)
+}
+BemVindo()
+// 9 Crie uma função que receba dois números e exiba a subtração deles.
+function Subtraçao(num1, num2) {
+console.log(&quot;Resultado da subtração:&quot;, num1 - num2)
+}
+Subtraçao(10, 11)
+// 10 Crie uma função chamada mostrarAnimalFavorito que exibe um animal.
+function MostrarAnimalFavorito() {
+console.log(&quot;Lobo&quot;)
+}
+MostrarAnimalFavorito()
+// 11 Crie uma função que exibe &quot;Estou aprendendo JavaScript!&quot;
+function Aprendendo() {
+console.log(&quot;Estou aprendendo JavaScript!&quot;)
+}
+Aprendendo()
+// 12 Crie uma função que recebe um número e exibe o triplo dele.
+function TriploDoNumero() {
+let numero = prompt(&quot;Digite um número: &quot;)
+let triploValor = Number(numero) * 3
+console.log(&quot;O triplo de &quot; + numero + &quot; é &quot; + triploValor)
+
+}
+TriploDoNumero()
+// 13 Crie uma função que recebe um nome e uma idade, e exibe &quot;Nome: [nome], Idade:
+[idade]&quot;.
+function DigiteSeuNomeEIdade() {
+
+console.log(&quot;Meu nome é Rodolfo e tenho 16 anos&quot;)
+}
+DigiteSeuNomeEIdade()
+// 14 Crie uma função que exibe os números de 1 a 10 no console.
+function MostreNumeros() {
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log(6)
+console.log(7)
+console.log(8)
+console.log(9)
+console.log(10)
+}
+MostreNumeros()
+// 15 Crie uma função que exibe “Aula de funções iniciada!”.
+function InicioDaAula() {
+console.log(&quot;A aula de funções começou&quot;)
+}
+InicioDaAula()
+// 16 Crie uma função que recebe um número e exibe “Você digitou o número [x]”
+function NumeroDigitadoFoi() {
+console.log(&quot;Você digitou o número: 31&quot;)
+
+}
+NumeroDigitadoFoi()
+// 17 Crie uma função que mostra o nome de um curso.
+function NomeDoCursoAtual() {
+let Curso = &quot;Lógica de Programação&quot;;
+console.log (Curso)
+}
+NomeDoCursoAtual();
+// 18 Crie uma função que simula uma chamada com seu nome.
+function fazerLigacao() {
+console.log(&quot;Alô quem é? sou o Rodolfo&quot;)
+}
+fazerLigacao(&quot;Rodolfo&quot;)
+// 19 Crie uma função que receba uma cor favorita e exiba: “Sua cor favorita é [cor]”.
+function minhaCorPreferida() {
+console.log(&quot;Minha cor favorita é Preto&quot;)
+}
+minhaCorPreferida()
+// 20 Crie uma função que receba um país e exiba: “Você mora no [país]”.
+function ondeEuMoro() {
+console.log(&quot;Eu moro no Brasil&quot;)
+}
+ondeEuMoro()
