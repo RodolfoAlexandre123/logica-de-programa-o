60b59d59c35ce04d436d28ac82579c9291112165
Rodolfo <rodolfoalexandre2009@gmail.com>
2025-06-03 09:14:20 -0300

    codigo.js

---
+++ Variaveis/codigo.js
@@ -0,0 +1,26 @@
+let nome = &quot;Rodolfo&quot;
+console.log(nome)
+let idade = 16
+console.log(idade)
+let aprendendoJavaScript = true
+console.log(aprendendoJavaScript)
+let nome2 = &quot;Rodolfo&quot;
+let sobrenome = &quot;Alexandre&quot;
+let nomecompleto = nome2 + &quot;&quot; + sobrenome
+console.log(nomecompleto)
+let num1 = 10
+let num2 = 20
+console.log(num1 + num2)
+let num3 = 30
+let num4 = 30
+console.log(num3*num4)
+let cidade = &quot;São Paulo&quot;
+let habitantes = 12300000
+console.log(&quot;Na cidade de&quot;,cidade, &quot;tem&quot;, habitantes, &quot;milhões de habitantes&quot;)
+let preco = 100
+preco += 10
+console.log(preco)
+let meuValor
+console.log(meuValor)
+let meuNulo = null
+console.log(meuNulo)
