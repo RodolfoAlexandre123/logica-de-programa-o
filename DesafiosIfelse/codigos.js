a03845022b2402090aedb342fd0509efafb7f0f4
Rodolfo <rodolfoalexandre2009@gmail.com>
2025-06-03 10:33:09 -0300

    codigos.js

---
+++ DesafiosIfelse/codigos.js
@@ -0,0 +1,162 @@
+let RespostaCorreta = 0
+let RespostaIncorreta = 0
+//Pergunta1
+console.log(&quot;Em que dia estamos? &quot;)
+console.log(&quot;A - 25/03/2025&quot;)
+console.log(&quot;B - 27/03/2027&quot;)
+console.log(&quot;C - 20/03/2025&quot;)
+console.log(&quot;D - 30/03/2020&quot;)
+let pergunta1 = prompt(&quot;Digite a alternativa correta: &quot;)
+if(pergunta1 == &quot;A&quot;|| pergunta1 ==&quot;25/03/2025&quot;){
+console.log(&quot;Alternativa correta!&quot;)
+RespostaCorreta ++
+}
+else{
+console.log(&quot;Alternativa errada! &quot;)
+RespostaIncorreta ++
+}
+
+//Pergunta2
+console.log(&quot;Em qual ano estamos? &quot;)
+console.log(&quot;A - 2050&quot;)
+console.log(&quot;B - 2025&quot;)
+console.log(&quot;C - 2030&quot;)
+console.log(&quot;D - 2060&quot;)
+let pergunta2 = prompt(&quot;Digite a alternativa correta: &quot;)
+if(pergunta2 == &quot;B&quot;|| pergunta2 ==&quot;2025&quot;){
+console.log(&quot;Alternativa correta!&quot;)
+RespostaCorreta ++
+}
+else{
+console.log(&quot;Alternativa errada! &quot;)
+
+RespostaIncorreta ++
+}
+//Pergunta3
+console.log(&quot;Qual o maior time da Inglaterra? &quot;)
+console.log(&quot;A - Liverpool&quot;)
+console.log(&quot;B - Manchester United&quot;)
+console.log(&quot;C - Chelsea&quot;)
+console.log(&quot;D - Manchester City&quot;)
+let pergunta3 = prompt(&quot;Digite a alternativa correta: &quot;)
+if(pergunta3 == &quot;A&quot;|| pergunta3 ==&quot;Liverpool&quot;){
+console.log(&quot;Alternativa correta!&quot;)
+RespostaCorreta ++
+}
+else{
+console.log(&quot;Alternativa errada! &quot;)
+RespostaIncorreta ++
+}
+//Pergunta4
+console.log(&quot;Qual o maior estado do Brasil? &quot;)
+console.log(&quot;A - São Paulo&quot;)
+console.log(&quot;B - Pará&quot;)
+console.log(&quot;C - Mato Grosso&quot;)
+console.log(&quot;D - Amazonas&quot;)
+let pergunta4 = prompt(&quot;Digite a alternativa correta: &quot;)
+if(pergunta4 == &quot;D&quot;|| pergunta4 ==&quot;Amazonas&quot;){
+console.log(&quot;Alternativa correta!&quot;)
+RespostaCorreta ++
+}
+else{
+console.log(&quot;Alternativa errada! &quot;)
+RespostaIncorreta ++
+
+}
+//Pergunta5
+console.log(&quot;Qual o animal mais perigoso do mundo? &quot;)
+console.log(&quot;A - Crocodilo&quot;)
+console.log(&quot;B - Tubarão-Branco&quot;)
+console.log(&quot;C - Mosquito&quot;)
+console.log(&quot;D - Água-Viva-Caixa&quot;)
+let pergunta5 = prompt(&quot;Digite a alternativa correta: &quot;)
+if(pergunta5 == &quot;C&quot;|| pergunta5 ==&quot;Mosquito&quot;){
+console.log(&quot;Alternativa correta!&quot;)
+RespostaCorreta ++
+}
+else{
+console.log(&quot;Alternativa errada! &quot;)
+RespostaIncorreta ++
+}
+//Pergunta6
+console.log(&quot;Qual o filme mais assistido do mundo? &quot;)
+console.log(&quot;A - Avatar&quot;)
+console.log(&quot;B - Vingadores: Ultimato&quot;)
+console.log(&quot;C - Avatar: O Caminho da Água&quot;)
+console.log(&quot;D - Titanic&quot;)
+let pergunta6 = prompt(&quot;Digite a alternativa correta: &quot;)
+if(pergunta6 == &quot;A&quot;|| pergunta6 ==&quot;Avatar&quot;){
+console.log(&quot;Alternativa correta!&quot;)
+RespostaCorreta ++
+}
+else{
+console.log(&quot;Alternativa errada! &quot;)
+RespostaIncorreta ++
+}
+
+//Pergunta7
+console.log(&quot;Qual o melhor atleta do futebol? &quot;)
+console.log(&quot;A - Lionel Messi&quot;)
+console.log(&quot;B - Pelé&quot;)
+console.log(&quot;C - Diego Maradona&quot;)
+console.log(&quot;D - Cristiano Ronaldo&quot;)
+let pergunta7 = prompt(&quot;Digite a alternativa correta: &quot;)
+if(pergunta7 == &quot;B&quot;|| pergunta7 ==&quot;Pelé&quot;){
+console.log(&quot;Alternativa correta!&quot;)
+RespostaCorreta ++
+}
+else{
+console.log(&quot;Alternativa errada! &quot;)
+RespostaIncorreta ++
+}
+//Pergunta8
+console.log(&quot;Qual o maior país em questão de terrítorio do mundo? &quot;)
+console.log(&quot;A - Estados Unidos&quot;)
+console.log(&quot;B - China&quot;)
+console.log(&quot;C - Canadá&quot;)
+console.log(&quot;D - Rússia&quot;)
+let pergunta8 = prompt(&quot;Digite a alternativa correta: &quot;)
+if(pergunta8 == &quot;D&quot;|| pergunta8 ==&quot;Rússia&quot;){
+console.log(&quot;Alternativa correta!&quot;)
+RespostaCorreta ++
+}
+else{
+console.log(&quot;Alternativa errada! &quot;)
+RespostaIncorreta ++
+}
+//Pergunta9
+
+console.log(&quot;Qual o menor país em questão de terrítorio do mundo? &quot;)
+console.log(&quot;A - Vaticano&quot;)
+console.log(&quot;B - Mônaco&quot;)
+console.log(&quot;C - Nauru&quot;)
+console.log(&quot;D - Tuvalu&quot;)
+let pergunta9 = prompt(&quot;Digite a alternativa correta: &quot;)
+if(pergunta9 == &quot;A&quot;|| pergunta9 ==&quot;Vaticano&quot;){
+console.log(&quot;Alternativa correta!&quot;)
+RespostaCorreta ++
+}
+else{
+console.log(&quot;Alternativa errada! &quot;)
+RespostaIncorreta ++
+}
+//Pergunta10
+console.log(&quot;Qual o país mais perigoso do mundo? &quot;)
+console.log(&quot;A - Jamaica&quot;)
+console.log(&quot;B - Honduras&quot;)
+console.log(&quot;C - Venezuela&quot;)
+console.log(&quot;D - Afeganistão&quot;)
+let pergunta10 = prompt(&quot;Digite a alternativa correta: &quot;)
+if(pergunta10 == &quot;C&quot;|| pergunta10 ==&quot;Venezuela&quot;){
+console.log(&quot;Alternativa correta!&quot;)
+RespostaCorreta ++
+}
+else{
+console.log(&quot;Alternativa errada! &quot;)
+RespostaIncorreta ++
+}
+//Exibir Resultado
+console.log(&quot;\n Resumo Final&quot;)
+
+console.log(&quot; Respostas Corretas: &quot; + RespostaCorreta)
+console.log(&quot; Respostas Incorretas: &quot; + RespostaIncorreta)
