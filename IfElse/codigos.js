79f0e7c96666847e04597191d012fdc2bea8d7c7
Rodolfo <rodolfoalexandre2009@gmail.com>
2025-06-03 10:27:32 -0300

    codigos.js

---
+++ IfElse/codigos.js
@@ -0,0 +1,126 @@
+Exercicios IF ELSE
+
+let idade = 16
+if (idade &gt;= 18){
+console.log(&quot;Maior de idade&quot;)
+} else {
+console.log(&quot;Menor de idade&quot;)
+}
+
+let hora = 14
+if (hora &gt;= 6 &amp;&amp; hora &lt; 12){
+console.log(&quot;Bom dia&quot;)
+} else if (hora &gt;= 12 &amp;&amp; hora &lt; 18) {
+console.log(&quot;Boa tarde&quot;)
+} else {
+console.log(&quot;Boa noite&quot;)
+}
+
+let idadeEvento = 16
+if (idadeEvento &lt; 16){
+console.log(&quot;Não pode entrar&quot;)
+} else {
+console.log(&quot;Pode entrar&quot;)
+}
+
+let a = 10, b = 9;
+if (a &gt; b){
+console.log(&quot; A é maior&quot;)
+} else{
+console.log(&quot;B é maior&quot;)
+
+}
+
+let temp = 18
+if (temp &lt; 15){
+console.log(&quot;Frio&quot;)
+} else if (temp &lt;= 25){
+console.log(&quot;Térmico&quot;)
+} else {
+console.log(&quot;Quente&quot;)
+}
+
+let palavra = &quot;Felicidade&quot;
+if (palavra === &quot;Felicidade&quot;){
+console.log(&quot;Palavra correta&quot;)
+} else{
+console.log(&quot;Palavra incorreta&quot;)
+}
+
+let moedas = 12
+if (moedas &lt; 10){
+console.log(&quot;Poucas moedas&quot;)
+} else if (moedas &lt;= 20){
+console.log(&quot;Quantidade média&quot;)
+} else{
+console.log(&quot;Muitas moedas&quot;)
+}
+
+let temperatura = 25
+if (temperatura &gt; 30){
+console.log(&quot;Muito quente&quot;)
+
+} else if (temperatura &gt;= 20){
+console.log(&quot;Calor&quot;)
+} else {
+console.log(&quot;Frio&quot;)
+}
+
+let num = -5
+if (num &gt;= 0){
+console.log(&quot;Número positivo&quot;)
+} else{
+console.log(&quot;Número negativo&quot;)
+}
+
+let nota = 8;
+if (nota &gt;= 7){
+console.log(&quot;Aprovado&quot;)
+} else{
+console.log(&quot;Reprovado&quot;)
+}
+
+let animal = &quot;Gato&quot;
+if (animal === &quot;Cachorro&quot; || animal === &quot;Gato&quot; || animal === &quot;Pássaro&quot;){
+console.log(&quot;Animal listado&quot;)
+} else{
+console.log(&quot;Animal não listado&quot;)
+}
+
+let diaSemana = &quot;Segunda-feira&quot;
+if (diaSemana === &quot;Sábado&quot; || diaSemana === &quot;Domingo&quot;){
+console.log(&quot;Fim de semana&quot;)
+
+} else{
+console.log(&quot;Dia útil&quot;)
+}
+
+let experiencia = 3
+if (experiencia &gt; 2){
+console.log(&quot;Experiente&quot;)
+} else{
+console.log(&quot;Iniciante&quot;)
+}
+
+let numero = 15
+if (numero &gt;= 10 &amp;&amp; numero &lt;= 20){
+console.log(&quot;Dentro do intervalo&quot;)
+} else{
+console.log(&quot;Fora do intervalo&quot;)
+}
+
+let idadeCNH = 18
+if (idadeCNH &gt;= 18){
+console.log(&quot;Pode tirar carteira de motorista&quot;)
+} else{
+console.log(&quot;Não pode tirar carteira de motorista&quot;)
+}
+
+let valor = 0
+if (valor &gt; 0){
+console.log(&quot;Positivo&quot;)
+} else if (valor &lt; 0){
+console.log(&quot;Negativo&quot;)
+
+} else{
+console.log(&quot;Zero&quot;)
