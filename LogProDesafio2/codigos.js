bc9ba0333c726ff466956f83160713e81102f798
Rodolfo <rodolfoalexandre2009@gmail.com>
2025-06-03 10:54:34 -0300

    codigos.js

---
+++ LogProDesafio2/codigos.js
@@ -0,0 +1,77 @@
+console.log("Faça seu login")
+let usuario = prompt("Digite seu login: ")
+let senha = prompt("Digite sua senha: ")
+
+if (usuario === "admin" && senha === "admin"){
+    console.log("Login efetuado com sucesso!")
+}
+
+else if (usuario === "user1" && senha === "user1"){
+    console.log("Login efetuado com sucesso!")
+}
+
+else if (usuario === "user2" && senha === "user2"){
+    console.log("Login efetuado com sucesso!")
+}
+
+else if (usuario === "user3" && senha === "user3"){
+    console.log("Login efetuado com sucesso!")
+}
+
+else if (usuario === "user4" && senha === "user4"){
+    console.log("Login efetuado com sucesso!")
+}
+
+else if (usuario === "user5" && senha === "user5"){
+    console.log("Login efetuado com sucesso!")
+}
+
+else if (usuario === "user6" && senha === "user6"){
+    console.log("Login efetuado com sucesso!")
+}
+
+else if (usuario === "user7" && senha === "user7"){
+    console.log("Login efetuado com sucesso!")
+}
+
+else if (usuario === "user8" && senha === "user8"){
+    console.log("Login efetuado com sucesso!")
+}
+
+else if (usuario === "user9" && senha === "user9"){
+    console.log("Login efetuado com sucesso!")
+}
+
+else{
+    console.log("Login invalido.")
+}
+
+
+
+function fazerlogin() {
+// Usuario e senha
+let usuariocorreto = "admin"
+let senhacorreta = "123456"
+
+//Digitar seus dados
+let usuario = prompt("Digite seu usuário: ")
+let senha = prompt("Digite sua senha: ")
+
+//Verificação de dados
+if (usuario === usuariocorreto && senha === senhacorreta) {
+    console.log("Login efetuado com sucesso")
+} else {
+    console.log("Usuário ou senha incorreto")
+} 
+}
+//Chamei a função
+fazerlogin()
+função.txt
+Exibindo função.txt…
+LOGPRO Desafio 2 - Funções 
+Matheus santos
+•
+27 de mai.
+100 pontos
+Comentários da turma
+
