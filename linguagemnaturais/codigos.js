aab28e7d8bb6e524a8068ea8715b1049df33eb6e
Rodolfo <rodolfoalexandre2009@gmail.com>
2025-06-03 09:28:35 -0300

    codigos.js

---
+++ linguagemnaturais/codigos.js
@@ -0,0 +1,99 @@
+Algoritmo Linguagem Natural
+
+1. Atividade - Fazer um arroz
+
+Antes de começar a fazer qualquer tipo de alimento na cozinha sempre se lave
+a mão, primeiro passo, lavar a mão para deixar ela limpa e não correr o risco
+de se contaminar com algum tipo de germe quando for fazer o arroz, pegar
+uma medida de 02 xícaras de 200ml de arroz, colocar no escorredor, lavar bem
+até a água branca sumir, ai deixar escorrer, com o arroz já limpo devemos
+escolher ele pois ele vem com algum arroz sujo ou até com alguma pedrinha o
+que pode prejudicar o gosto do arroz e com tempo a saúde, logo após pegue
+uma panela pequena, coloque um fio de azeite e descasque 02 dentes de alho,
+pique bem ele e coloque em cima do azeite, aí pegaremos uma tábua de corte
+e ralamos uma cenoura e colocamos em cima do arroz. Agora ligue a panela
+em fogo baixo e refogue esse alho até pegar uma cor, depois coloque o arroz
+escolhido junto com a cenoura ralada e mexa bem para o alho pegar o gosto
+no arroz. E no final do preparo colocar 2 xicaras e meia de água, mexe
+bastante e na panela coloque sal a gosto. Em fogo médio, com a tampa meio
+aberta vai ferver e depois que a água baixar e fazer uns buraquinhos no arroz
+você abaixa o fogo e mantém a tampa para ver se a água secou, com uma
+colher faz um buraco até o final da panela para ver se a água secou, se tiver
+seco só desligar o fogo e se servir. Assim seu arroz estará pronto, lembrando
+que terá que esperar ele esfriar para assim colocar o arroz em recipiente da
+sua preferência e ele já em uma temperatura baixa colocar ele na geladeira
+para evitar que estrague.
+
+2. Atividade - Fazer um chá
+
+Há dois tipo de chás, o que você normalmente compra no mercado ou o que
+você colhe para fazer, para o chá natural ser feito precisaremos utilizar o boldo,
+uma caneca e uma xicara, coloque em torno de 150 há 200ml de agua na
+caneca, ligue em fogo médio até ferver a água em seguida pegue uma colher
+de chá de ervas secas (boldo) e coloque na caneca, deixe por 10 minutos e
+depois do chá pronto coloque na xicara e estará pronto.
+
+3. Atividade – Preparar um sanduiche
+
+Para preparar um bom sanduiche terá que ter os seguintes ingredientes, pão
+de sal (ou da sua preferência), presunto, queijo, maionese, salada e tomate.
+Corte o pão de sal com uma faca de serra, com o pão cortado você escolhe se
+deseja tirar ou deixar o miolo do pão, coloque o pão até uma frigideira e nela
+coloque manteiga e deixe derreter, com a manteiga derretida coloque o pão e
+vá esfregando para que ele fique dourado e pegue um gosto a mais, em
+seguida com o pão já dourado passe a maionese da sua preferencia e na
+quantidade que preferir, coloque o presunto, o queijo, salada e tomate e seu
+sanduiche estará feito, para se servir você pode comer o pão inteiro ou partir
+ao meio.
+
+4. Atividade – Comprar ingresso online
+
+Para comprar um ingresso online primeiro abra o navegador ou aplicativo da
+plataforma de venda de ingressos, digital o URL do site ou buscar pelo nome
+da loja de aplicativos. Com o site aberto vá até a barra de pesquisa ou navegue
+pelas categorias disponíveis para achar o evento desejável, depois de
+encontrar o evento verifique as opções de ingressos disponíveis (pista, VIP,
+meia-entrada, inteira, etc.) e escolha a quantidade de ingressos, confirme os
+ingressos escolhidos no carrinho para poder pagar, caso tenha uma conta só
+fazer o login caso não eles iram te pedir para criar para de avisarem sobre sua
+compra e sobre o evento, Verifique se os ingressos, quantidades e valores
+estão corretos e confira a taxa de serviço e possíveis descontos. Selecione a
+opção de pagamento (cartão de crédito, débito, PIX, boleto, etc.) e insira os
+dados do pagamento. Com os dados preenchidos finalize a compra e aguarde
+a confirmação do pagamento, depois de um tempo verifique seu e-mail ou área
+do usuário no site para baixar o ingresso e caso necessário imprimir ou salvar o
+QR code para apresentar na entrada do evento.
+
+5. Atividade – Organizar a mochila para a escola
+
+Para preparar a mochila se deve pensar no dia de aula, leve a mochila, um
+estojo com lápis, borracha, canetas, apontador, tesoura, cola, lápis de cor. Um
+caderno, uma garrafa de água, um guarda-chuva e uma sacola para guarda ele
+caso ele esteja úmido e chaves de casa.
+
+6. Atividade – Arrumar a cama
+
+O momento de arrumar a cama é logo depois de acordar, vamos levar em
+conta a cama de casal então primeiro deve se tirar o edredom ou o lençol que
+você estava se cobrindo e esticar a colcha para que fique reta e sem ondas,
+com a colcha já esticada coloque o lençol em cima de maneira que fique com
+igual todos lados e não que tenha mais de um lado no que no outro, com o
+lençol arrumado na cama coloque os travesseiros de maneira inclinada na
+cama e assim você terá uma cama arrumada.
+
+7. Atividade – Fazer um suco
+
+Para fazer um suco há como opção fazer natural ou comprar um pó, para se
+fazer o natural precisaremos de 3 há 4 frutas, liquidificador, jarra, uma faca e
+açúcar. Com a faca descasque a fruta e coloque no liquidificador, no
+liquidificador bata até ficar liquido, já liquido coloque na jarra e se precisar
+coloque açúcar na quantidade que preferir, depois de feito misture com uma
+colher para que o açúcar e o suco se misturem e fique mais saboroso.
+
+8. Atividade – Lavar louça
+
+Dependendo da louça pode ser demorada ou não, vamos pegar a mais curta
+como a do café da manhã, na louça há, uma caneca, um pratinho, uma colher,
+uma faca de passar manteiga, uma faca de serra e um pote. Antes de lavar
+coloque detergente na esponja e coloque um pouco de água para fazer
+espuma, a esponja já com esponja comece ensaboando o prato, caneca,
